7a4baa16327aea3c641346de730f238394da64a1
Knight <71361121+knightdoc@users.noreply.github.com>
2025-04-09 19:23:15 -0400

    Update hello.py
    print welcome to GitHub added

--- hello.py
+++ hello.py
@@ -2,4 +2,21 @@
 #4/9/2025
 #A simple pythong program to print a message
 
+#Aaron Timm
+#4/9/2025
+#added a new message
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
+
+
+
+
+
+
+
+
+
+
+
